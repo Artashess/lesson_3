87cda581987f73c3e6acae58eba6dbc043cbaabd
Your Name <you@example.com>
2018-12-04 17:32:26 +0400

    lesson3

---
+++ index.js
@@ -0,0 +1,25 @@
+// var os = require("os");
+// var message = "The platform is ";
+
+// function main(){
+//    console.log(message + os.platform());
+// }
+// main();
+var express = require("express");
+var app = express();
+app.get("/", function(req, res){
+    res.send("<h1>Hello world</h1>");
+ }); 
+app.get("/name/:name", function(req, res){
+    var name = req.params.name;
+    res.send("<h1>Hello " + name + "</h1>");
+ });
+ app.get("/name/:google", function(req, res){
+    var google = req.params.google;
+    res.send(res.redirect('http://google.com'));
+ });
+ 
+ app.listen(3000, function(){
+    console.log("Example is running on port 3000");
+ });
+ 
